66c111c3d184bc40e7b5bb56b444a44fc79eb714
Vava <151790869+vaijogar@users.noreply.github.com>
2026-03-06 12:16:26 -0400

    Image upload test

--- image/demo/src/main/java/io/spring/image/demo/controller/ImagesController.java
+++ image/demo/src/main/java/io/spring/image/demo/controller/ImagesController.java
@@ -1,5 +1,19 @@
 package io.spring.image.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/upload")
+@Slf4j
+
 public class ImagesController {
     //*
     // {"name": "", "size":100} //application/json
